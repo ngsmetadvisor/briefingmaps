0fdde897e1e4f4f4dcfb806ab087ed1d8b3a48d1
ngsmetadvisor <ngsmetadvisor@gmail.com>
2026-06-09 22:19:14 -0600

    try LLJ + 2 timestep a day

--- forecast_gem_sfc_map.py
+++ forecast_gem_sfc_map.py
@@ -4790,8 +4790,6 @@ print(f"  UA export hour: {_ua_export_hour:02d}Z")
 
 _time_steps = []
 for (_date_val, _hr) in _synoptic_times:
-    if int(_hr) != _ua_export_hour:
-        continue
     _date_str = pd.Timestamp(_date_val).strftime('%Y%m%d')
     _key      = f'{_date_str}_{int(_hr):02d}'
     _dt       = pd.Timestamp(_date_val).date()
